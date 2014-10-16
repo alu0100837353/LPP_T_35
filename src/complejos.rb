980ec882d09cb7a773e8a4601af6c5b3031ddf69
alu0100837353 <alu0100837353@ull.edu.es>
2014-10-16 16:36:25 +0000

    Añadidas suma, resta, multiplicacion

--- src/complejos.rb
+++ src/complejos.rb
@@ -14,4 +14,24 @@ class Complejo
             "#{@real} #{@imaginaria}i"
         end    
     end
+    
+    def +(suma)
+       
+        return Complejo.new(@real+suma.real, @imaginaria+suma.imaginaria) if suma.is_a? Complejo
+    
+    end
+    
+    def -(resta)
+       
+        return Complejo.new(@real-resta.real, @imaginaria+resta.imaginaria) if resta.is_a? Complejo
+    
+    end
+    
+    def *(multiplicacion)
+       
+        return Complejo.new((@real*multiplicacion.real)-(@imaginaria*multiplacacion.imaginaria),(@real*multiplicacion.imaginaria)+(@imaginaria*multiplicacion.real)) if multiplicacion.is_a? Complejo
+    
+    end
+    
+    
 end
